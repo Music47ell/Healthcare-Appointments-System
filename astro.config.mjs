7b7f45e42bcc787b884315005bec06f9fa108379
Ahmet ALMAZ <Music47ell@gmail.com>
2024-06-03 17:32:48 +0300

    chore: switch to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://ksu-gp-has.news47ell.com",
   base: "/",
   trailingSlash: "never",
-  output: "static",
+  output: "hyprid",
   integrations: [tailwind()],
+  adapter: cloudflare(),
 });
